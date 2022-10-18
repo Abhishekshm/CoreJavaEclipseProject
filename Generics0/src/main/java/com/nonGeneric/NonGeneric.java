a3ee21ed9975b74295b383afb56af5ada11a39f9
Abhishek <Abhishek@Abhishek>
2022-10-18 16:36:07 +0530

    Non Generics

--- Generics0/src/main/java/com/nonGeneric/NonGeneric.java
+++ Generics0/src/main/java/com/nonGeneric/NonGeneric.java
@@ -1,19 +1,29 @@
 package com.nonGeneric;
 
+/*
+Program :- Creating Non Generic Class and showing type of object
+@Author :- Abhishek Sharma
+Date :-18/10/2022
+*/
+//declaring class
 public class NonGeneric {
 
-	Object obj;
+	Object obj; // creating instance of object
+
+	// Default constructor
 
 	public NonGeneric() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	// paramaetrized Constructor
 	public NonGeneric(Object obj) {
 		super();
 		this.obj = obj;
 	}
 
+	// -----------Getter and Setter-----------///
 	public Object getObj() {
 		return obj;
 	}
@@ -22,7 +32,8 @@ public class NonGeneric {
 		this.obj = obj;
 	}
 
+	// Method for showing type of object
 	void typeOfObject() {
 		System.out.println("Type: " + obj.getClass().getName());
-	}
-}
+	} // end of method
+}// end of class
--- Generics0/src/main/java/com/nonGeneric/NonGenericDemo.java
+++ Generics0/src/main/java/com/nonGeneric/NonGenericDemo.java
@@ -1,22 +1,29 @@
 package com.nonGeneric;
 
+//declaring class
 public class NonGenericDemo {
 
+	// declaring main method
 	public static void main(String[] args) {
 
+		// creating Nongeneric object and passing integer value
 		NonGeneric iObj = new NonGeneric(55);
-		iObj.typeOfObject();
+		iObj.typeOfObject(); // calling method
 
-		int v = (Integer) iObj.getObj();
-		System.out.println(v);
+		int v = (Integer) iObj.getObj(); // Converting type of object which is passed
+		System.out.println(v); // printing value
 
+		// creating Nongeneric object and passing String value
 		NonGeneric strObj = new NonGeneric("Java");
-		strObj.typeOfObject();
+		strObj.typeOfObject(); // calling method
 
-		String str = (String) strObj.getObj();
-		System.out.println(str);
+		String str = (String) strObj.getObj(); // Converting type of object which is passed
+		System.out.println(str); // printing value
 
-		iObj = strObj; // it is allowed in non generic because there is no type safty between String
-						// and Integer
-	}
-}
+		iObj = strObj; // it is allowed in non generic
+		// Because there is no type safty
+		// In this class Integer and String vlaues are treted in same way
+		// you can also equals two value
+
+	} // end of main
+}// end of class
